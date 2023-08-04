0dd98663ac0f8a332887ccbc0b21227c58b73688
priyank_009 <86396567+Priyank009@users.noreply.github.com>
2023-08-04 22:47:27 +0530

    Add files via upload

---
+++ Bank Managemant System/src/bank/managemant/system/Conn.java
@@ -0,0 +1,22 @@
+package bank.managemant.system;
+
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+//            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Priyank");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+} 
+
